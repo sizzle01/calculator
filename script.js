09c323257d3754712f5307c2f7516062345a3799
murtala Aliyu <talktoaliyu@gmail.com>
2020-06-05 15:07:36 +0100

    adding logic

--- script.js
+++ script.js
@@ -5,4 +5,36 @@ function getHistory() {
 function printHistory(num) {
     return (document.getElementById("history-value").innerText = num);
 }
-(printHistory("973");
+
+function printOutPut(num) {
+    if (num == "") {
+        document.getElementById("output-value").innertext = num;
+    } else {
+        document.getElementById("output-value").innerText = getFormattedNumber(num);
+    }
+}
+
+function getFormattedNumber(num) {
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g, ""));
+}
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener("click", function() {});
+}
+var number = document.getElementsByClassName("number");
+for (var i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", function() {
+        var output = reverseNumberFormat(getOutPut());
+        if (output != NaN) {
+            //if output is a number
+            output = output + this.id;
+            printOutPut(output);
+        }
+    });
+}
